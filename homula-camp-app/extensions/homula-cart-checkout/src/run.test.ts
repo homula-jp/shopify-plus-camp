fce6e5eb2dc567784e2471b2dd3a6155e8ca8ecb
Naoya Kashida <naoya.kashida@homula.jp>
2023-11-27 15:07:07 +0900

    Function作成

---
+++ homula-camp-app/extensions/homula-cart-checkout/src/run.test.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from "vitest";
+import { run } from "./run";
+import { FunctionRunResult } from "../generated/api";
+
+describe("cart checkout validation function", () => {
+  it("returns an error when quantity exceeds one", () => {
+    const result = run({
+      cart: {
+        lines: [
+          {
+            quantity: 1,
+          },
+        ],
+      },
+    });
+    const expected: FunctionRunResult = {
+      errors: [
+        {
+          localizedMessage: "2個以上の商品をカートに入れてください",
+          target: "cart",
+        },
+      ],
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  it("returns no errors when quantity is one", () => {
+    const result = run({
+      cart: {
+        lines: [
+          {
+            quantity: 2,
+          },
+        ],
+      },
+    });
+    const expected: FunctionRunResult = { errors: [] };
+
+    expect(result).toEqual(expected);
+  });
+});

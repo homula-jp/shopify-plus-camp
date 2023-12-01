ccb3b19bbee46ef64dc4fee10bfef67070d77443
Naoya Kashida <naoya.kashida@homula.jp>
2023-12-01 12:48:08 +0900

    マルチパスログイン実装

--- homula-camp-app2/app/routes/app.multipass.tsx
+++ homula-camp-app2/app/routes/app.multipass.tsx
@@ -1,4 +1,5 @@
 import { json, redirect } from "@remix-run/node";
+import { v4 as uuid } from "uuid";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import type { SubmitFunction } from "@remix-run/react";
 import {
@@ -13,8 +14,6 @@ import {
   Card,
   Form,
   Layout,
-  Link,
-  List,
   Page,
   TextField,
 } from "@shopify/polaris";
@@ -22,23 +21,29 @@ import type { FormEvent } from "react";
 import { useCallback, useState } from "react";
 import { authenticate } from "~/shopify.server";
 import type { Data } from "~/types/remix";
+import db from "~/db.server";
 
 const getSecret = async (request: ActionFunctionArgs["request"]) => {
-  const { admin } = await authenticate.admin(request);
-  const response = await admin.graphql(
-    `#graphql
-      query {
-        shop {
-          id
-          metafield(namespace: "multipass", key: "secret") {
-            value
-          }
-        }
-      }
-    `
-  );
-  const responseJson = await response.json();
-  return responseJson.data.shop.metafield.value;
+  const shop = await getShopId(request);
+  const shopRecord = await db.multipassSecret.findFirst({
+    where: { shop },
+  });
+  return shopRecord?.secret ?? undefined;
+  // const { admin } = await authenticate.admin(request);
+  // const response = await admin.graphql(
+  //   `#graphql
+  //     query {
+  //       shop {
+  //         id
+  //         metafield(namespace: "multipass", key: "secret") {
+  //           value
+  //         }
+  //       }
+  //     }
+  //   `
+  // );
+  // const responseJson = await response.json();
+  // return responseJson.data.shop.metafield.value;
 };
 
 const getShopId = async (request: ActionFunctionArgs["request"]) => {
@@ -61,57 +66,84 @@ const updateMultipassSecret = async (
   shopId: string,
   secret: string
 ) => {
-  const { admin } = await authenticate.admin(request);
-  const response = await admin.graphql(
-    `#graphql
-      mutation metafieldsSet($metafields: [MetafieldsSetInput!]!) {
-        metafieldsSet(metafields: $metafields) {
-          metafields {
-            key
-            value
-          }
-          userErrors {
-            field
-            message
-          }
-        }
-      }
-    `,
-    {
-      variables: {
-        metafields: [
-          {
-            key: "secret",
-            namespace: "multipass",
-            ownerId: shopId,
-            type: "single_line_text_field",
-            value: secret,
-          },
-        ],
-      },
-    }
-  );
+  const record = await db.multipassSecret.findFirst({
+    where: { shop: shopId },
+  });
+  if (record) {
+    const newRecord = await db.multipassSecret.update({
+      where: { id: record?.id },
+      data: { secret },
+    });
+    return newRecord.secret;
+  }
+  const newRecord = await db.multipassSecret.create({
+    data: { id: uuid(), shop: shopId, secret },
+  });
+  return newRecord;
+  // const { admin } = await authenticate.admin(request);
+  // const response = await admin.graphql(
+  //   `#graphql
+  //     mutation metafieldsSet($metafields: [MetafieldsSetInput!]!) {
+  //       metafieldsSet(metafields: $metafields) {
+  //         metafields {
+  //           key
+  //           value
+  //         }
+  //         userErrors {
+  //           field
+  //           message
+  //         }
+  //       }
+  //     }
+  //   `,
+  //   {
+  //     variables: {
+  //       metafields: [
+  //         {
+  //           key: "secret",
+  //           namespace: "multipass",
+  //           ownerId: shopId,
+  //           type: "single_line_text_field",
+  //           value: secret,
+  //         },
+  //       ],
+  //     },
+  //   }
+  // );
 
-  const responseJson = await response.json();
+  // const responseJson = await response.json();
 
-  return json(responseJson.data.metafieldsSet);
+  // return json(responseJson.data.metafieldsSet);
 };
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
   const formData = await request.clone().formData();
   const shopId = await getShopId(request);
+  const _action = formData.get("_action") as string;
   const secret = formData.get("secret") as string;
 
-  return await updateMultipassSecret(request, shopId, secret);
+  if (_action === "UPDATE_SECRET") {
+    return await updateMultipassSecret(request, shopId, secret);
+  }
+
+  if (_action === "LOGIN") {
+    const { redirect } = await authenticate.admin(request);
+    const formData = await request.clone().formData();
+    const url = formData.get("url") as string;
+    return redirect(url, { target: "_parent" });
+  }
 };
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return await getSecret(request);
+  return json({
+    secret: await getSecret(request),
+    shopId: await getShopId(request),
+  });
 };
 
 export default function Multipass() {
   const data = useLoaderData<typeof loader>() as Data<typeof loader>;
-  const [secret, setSecret] = useState(data);
+  const [secret, setSecret] = useState(data.secret ?? "");
   const nav = useNavigation();
   const submit = useSubmit();
   const isLoading =
@@ -133,33 +165,48 @@ export default function Multipass() {
         <Card>
           <Layout>
             <Layout.Section>
-              <Form onSubmit={onSubmit}>
-                <BlockStack gap="500">
-                  <TextField
-                    label="Multipass secret"
-                    value={secret}
-                    name="secret"
-                    onChange={setSecret}
-                    autoComplete="off"
-                  />
-                  <div>
+              <BlockStack gap="500">
+                <Form onSubmit={onSubmit}>
+                  <BlockStack gap="500">
+                    <TextField
+                      label="Multipass secret"
+                      value={secret}
+                      name="secret"
+                      onChange={setSecret}
+                      autoComplete="off"
+                    />
+                    <div>
+                      <input
+                        type="hidden"
+                        name="_action"
+                        value="UPDATE_SECRET"
+                      />
+                      <Button submit variant="primary" loading={isLoading}>
+                        Secretを保存
+                      </Button>
+                    </div>
+                  </BlockStack>
+                </Form>
+                <div>
+                  <Form
+                    onSubmit={(event) => {
+                      const formData = new FormData(event.currentTarget);
+                      const shop = new URLSearchParams(
+                        window.location.search
+                      ).get("shop");
+                      const url = `https://${window.location.hostname}/multipass?login_shop=${shop}&shop_id=${data.shopId}`;
+                      formData.append("url", url);
+                      console.log(url);
+                      submitAction(formData);
+                    }}
+                  >
+                    <input type="hidden" name="_action" value="LOGIN" />
                     <Button submit variant="primary" loading={isLoading}>
-                      Secretを保存
+                      ログインページを開く
                     </Button>
-                  </div>
-                  <div>
-                    <Link
-                      onClick={() => {
-                        redirect(
-                          `https://${window.location.hostname}/multipass?login_shop=${shop}`
-                        );
-                      }}
-                    >
-                      ログインページ
-                    </Link>
-                  </div>
-                </BlockStack>
-              </Form>
+                  </Form>
+                </div>
+              </BlockStack>
             </Layout.Section>
           </Layout>
         </Card>
@@ -168,14 +215,7 @@ export default function Multipass() {
             <Layout>
               <Layout.Section>
                 <BlockStack gap="500">
-                  {actionData.metafields.map((metafield: any) => (
-                    <div key={metafield.key}>
-                      {metafield.key}を更新しました。
-                    </div>
-                  ))}
-                  {actionData.userErrors.map((error: any) => (
-                    <div key={error.field}>{error.message}</div>
-                  ))}
+                  {actionData && <div>Secretを更新しました。</div>}
                 </BlockStack>
               </Layout.Section>
             </Layout>
---
+++ homula-camp-app2/app/routes/multipass.tsx
@@ -0,0 +1,223 @@
+import { json, redirect } from "@remix-run/node";
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import indexStyles from "./_index/style.css";
+import db from "~/db.server";
+import crypto from "crypto";
+import type { Data } from "~/types/remix";
+
+const getSecret = async (
+  request: ActionFunctionArgs["request"],
+  shop: string
+) => {
+  const shopRecord = await db.multipassSecret.findFirst({
+    where: { shop },
+  });
+  return shopRecord?.secret ?? undefined;
+  // const response = await fetch(
+  //   `https://${shop}/admin/api/2023-10/metafields.json`,
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "X-Shopify-Access-Token": API_SECRET,
+  //     },
+  //   }
+  // );
+  // const result = await response.json();
+
+  // const { admin } = await authenticate.admin(request);
+  // const response = await admin.graphql(
+  //   `#graphql
+  //     query {
+  //       shop {
+  //         id
+  //         metafield(namespace: "multipass", key: "secret") {
+  //           value
+  //         }
+  //       }
+  //     }
+  //   `
+  // );
+  // const responseJson = await response.json();
+  // return responseJson.data.shop.metafield.value;
+};
+
+const generateMultipassToken = function (json: any, secret: string) {
+  const str = JSON.stringify(json);
+  const keyMaterial = crypto.createHash("sha256").update(secret).digest();
+  const encryptionKey = keyMaterial.slice(0, 16);
+  const signatureKey = keyMaterial.slice(16, 32);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-128-cbc", encryptionKey, iv);
+  const cipherText = Buffer.concat([
+    iv,
+    cipher.update(str, "utf8"),
+    cipher.final(),
+  ]);
+  const signed = crypto
+    .createHmac("SHA256", signatureKey)
+    .update(cipherText)
+    .digest();
+  const token = Buffer.concat([cipherText, signed])
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+  console.log(`generateMultipassToken token ${token}`);
+  return token;
+};
+
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+  const formData = await request.clone().formData();
+  const shop = formData.get("shop") as string;
+  const shopId = formData.get("shopId") as string;
+
+  const email = formData.get("email") as string;
+  const identifier = formData.get("identifier") as string;
+  const first_name = formData.get("first_name") as string;
+  const last_name = formData.get("last_name") as string;
+  const tag_string = formData.get("tag_string") as string;
+  const remote_ip = formData.get("remote_ip") as string;
+  const return_to = formData.get("return_to") as string;
+  const multipassJson = {
+    email,
+    identifier,
+    first_name,
+    last_name,
+    tag_string,
+    remote_ip,
+    return_to,
+    created_at: new Date().toISOString(),
+  };
+
+  const secret = await getSecret(request, shopId);
+  const token = generateMultipassToken(multipassJson, secret ?? "");
+
+  const url = `https://${shop}/account/login/multipass/${token}`;
+  return redirect(url);
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  return json({
+    shop: url.searchParams.get("shop"),
+    shopId: url.searchParams.get("shop_id"),
+  });
+};
+
+// export const links = () => [{ rel: "stylesheet", href: indexStyles }];
+
+export default function Multipass() {
+  const data = useLoaderData<typeof loader>() as Data<typeof loader>;
+
+  return (
+    <div className="index">
+      <div className="content">
+        <h1>Multipass test</h1>
+        <Form method="post" action="/multipass">
+          <fieldset>
+            <div className="input">
+              <label htmlFor="email">
+                <b>Email*:</b>
+              </label>
+              <br />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="test@example.com"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="identifier">Identifier:</label>
+              <br />
+              <input
+                type="text"
+                name="identifier"
+                id="identifier"
+                placeholder="nic123"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="password">Password:</label>
+              <br />
+              <input
+                type="text"
+                name="password"
+                id="password"
+                disabled={true}
+                value="Note that you don't have to give your password to Multipass!"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="first_name">First name:</label>
+              <br />
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                placeholder="Nic"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="last_name">Last name:</label>
+              <br />
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                placeholder="Potts"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="tag_string">Tags separated by ",":</label>
+              <br />
+              <input
+                type="text"
+                name="tag_string"
+                id="tag_string"
+                placeholder="canadian, premium"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="remote_ip">Remote IP:</label>
+              <br />
+              <input
+                type="text"
+                name="remote_ip"
+                id="remote_ip"
+                placeholder="107.20.160.121"
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="return_to">URL to return:</label>
+              <br />
+              <input
+                type="text"
+                name="return_to"
+                id="return_to"
+                placeholder="http://your_store_or_some_specific_site"
+              />
+            </div>
+          </fieldset>
+          <p>
+            <button type="submit">ログイン</button>
+          </p>
+          <input
+            type="hidden"
+            name="shop"
+            value={(data.shop as string) ?? undefined}
+            id="shop"
+          />
+          <input
+            type="hidden"
+            name="shopId"
+            value={(data.shopId as string) ?? undefined}
+            id="shopId"
+          />
+        </Form>
+      </div>
+    </div>
+  );
+}
